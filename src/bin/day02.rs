aeb6d4f723a75343c5b0f12cca1869f59e81c243
Katherine Rosenfeld <krosenf@gmail.com>
2025-11-11 18:22:43 -0800

    part b of day 2

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,8 +1,87 @@
 use std::fs;
 
-fn main() -> std::io::Result<()> {
-    let contents = fs::read_to_string("inputs/day02.txt")?;
+// Return True if not safe and False if safe
+fn check_row(row: &Vec<i64>) -> bool {
+    let row_length = row.len();
+    let mut is_row_not_safe = false;
+    let mut req = 'n';
+    for i in 0..(row_length - 1) {
+        // get element difference
+        let diff = row[i + 1] - row[i];
 
+        // check whether the difference is allowed (0 or |d| < 4)
+        if diff == 0 {
+            is_row_not_safe = true;
+        } else if diff.abs() > 3 {
+            is_row_not_safe = true;
+        }
+        // set increasing or decreasing requirement
+        if i == 0 {
+            if diff > 0 {
+                req = 'i';
+            } else {
+                req = 'd';
+            }
+        } else {
+            if (diff < 0) & (req == 'i') {
+                is_row_not_safe = true;
+            } else if (diff > 0) & (req == 'd') {
+                is_row_not_safe = true;
+            }
+        }
+    }
+    is_row_not_safe
+}
+
+// Part A
+fn part_a(rows: &Vec<Vec<i64>>) {
+    let mut counter = 0;
+    for row in rows {
+        let is_row_not_safe = check_row(&row);
+        // Check flag, if true then not safe
+        if !is_row_not_safe {
+            counter = counter + 1;
+        }
+    }
+    println!("Number of safe reports: {}", counter); // Ans: 421
+}
+
+fn part_b(rows: &Vec<Vec<i64>>) {
+    let mut counter = 0;
+    for row in rows {
+        // first check the row
+        let mut is_row_not_safe = check_row(&row);
+        // if not safe then we should apply the problem dampener
+        if is_row_not_safe {
+            let row_length = row.len();
+            // Iterate over all levels to remove
+            for i in 0..row_length {
+                let mut new_row = Vec::new();
+                // Fill in the new_row array, skipping i
+                for j in 0..row_length {
+                    if j != i {
+                        new_row.push(row[j]);
+                    }
+                }
+                // Now check this row to see if now safe, remember if check_row is true then not safe
+                let is_new_row_not_safe = check_row(&new_row);
+                if !is_new_row_not_safe {
+                    is_row_not_safe = false;
+                    break;
+                }
+            }
+        }
+        // Check flag to count if safe, if true then not safe
+        if !is_row_not_safe {
+            counter = counter + 1;
+        }
+    }
+    println!("Number of safe rows: {}", counter)
+}
+
+fn main() -> std::io::Result<()> {
+    // Read in the data
+    let contents = fs::read_to_string("inputs/day02.txt")?;
     let rows: Vec<Vec<i64>> = contents
         .lines()
         .map(|line| {
@@ -12,40 +91,11 @@ fn main() -> std::io::Result<()> {
         })
         .collect();
 
-    let mut counter = 0;
-    for row in &rows {
-        let row_length = row.len();
-        let mut flag = false;
-        let mut req = 'n';
-        for i in 0..(row_length - 1) {
-            // get element difference
-            let diff = row[i + 1] - row[i];
-            // check whether the difference is allowed (0 or |d| < 4)
-            if diff == 0 {
-                flag = true;
-            } else if diff.abs() > 3 {
-                flag = true;
-            }
-            // set increasing or decreasing requirement
-            if i == 0 {
-                if diff > 0 {
-                    req = 'i';
-                } else {
-                    req = 'd';
-                }
-            } else {
-                if (diff < 0) & (req == 'i') {
-                    flag = true;
-                } else if (diff > 0) & (req == 'd') {
-                    flag = true;
-                }
-            }
-        }
-        // Check flag, if true that not safe
-        if !flag {
-            counter = counter + 1;
-        }
-    }
-    println!("Number of safe reports: {}", counter);
+    // Run part A
+    part_a(&rows);
+
+    // Run part B
+    part_b(&rows);
+
     Ok(())
 }
